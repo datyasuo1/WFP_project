356fdf77688444a29d3d1f80070ce46beb20c24a
LeDinhCuong999 <73331487+LeDinhCuong999@users.noreply.github.com>
2021-11-19 16:09:15 +0700

    ...

---
+++ AppProject/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+namespace AppProject.Models
+{
+    class Product
+    {
+        public int id { get; set; }
+        public string ProductName { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public BitmapImage Img { get; set; }
+    }
+}
