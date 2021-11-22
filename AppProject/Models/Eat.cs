19d422373632b2aa9aabedc092314a32fd737fec
datyasuo1 <73332200+datyasuo1@users.noreply.github.com>
2021-11-22 19:20:19 +0700

    new

---
+++ AppProject/Models/Eat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppProject.Models.Enity;
+
+
+namespace AppProject.Models
+{
+    class Eat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public Category Eatss { get; set; }
+
+      
+    }
+}
---
+++ AppProject/Pages/Home.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using AppProject.Models;
+using AppProject.Services;
+using  AppProject.Models.Enity;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppProject.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Home : Page
+    {
+        public Home()
+        {
+            this.InitializeComponent();
+            //var p1 = new HomeCate() { Name = "quan ao thoi trang thu dong", Price = 200000, Des = "haha", Img = new BitmapImage(new Uri("https://massageishealthy.com/wp-content/uploads/2019/06/hinh-anh-do-an-hinh-anh-mon-an-thuc-an-ngon-dep-viet-nam-the-gioi-4.png")) };
+            //lấy ảnh từ trong thư mục 
+            //var p2 = new HomeCate() { Name = "quan ao thoi trang thu dong", Price = 200000, Des = "haha", Img = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png")) };
+            //Homes.Items.Add(p1);
+            //Homes.Items.Add(p2);
+            RenderSpecial();
+        }
+
+        public async void RenderSpecial()
+        {
+            // chi viec goi object cuar ApiService vao dung
+            ApiService apiService = new ApiService();
+            todaySpecial food = await apiService.GetTodaySpecial();
+            if (food != null)
+            {
+                foreach (var c in food.data)
+                {
+                    Homes.Items.Add(new Product() { Name = c.name, Des = c.description, ID = c.id, Img = c.image, Price = c.price });
+                }
+            }
+        }
+
+        private void Button_Tapped(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    }
+
